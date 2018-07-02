58b6ac203e2bea7b5eb8306d614575ecd21cce73
naveenkaram <navkar@outlook.com>
2018-07-02 18:36:22 -0400

    Files

--- files.py
+++ files.py
@@ -1,6 +1,17 @@
 def main():
-    # w+ = write and create 
+    # w+ = write and create, a = append only
     f = open("newfile.txt", "w+")
-    
+    #f = open("newfile.txt", "a")
     for i in range(1,100):
         f.write("This is on line " + str(i) + "\r\n")
+
+    f.close()
+    # Read and print out the contents of the file
+    f = open("newfile.txt", "r")
+    if (f.mode == 'r'):
+        contents = f.read()
+        print(contents)
+
+
+if __name__ == "__main__":
+    main()
